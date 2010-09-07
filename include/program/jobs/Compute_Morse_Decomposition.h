5a2111ff8523f3298ceb77225b67476e63fae7c8
Pawel Pilarczyk <paw.pilar@gmail.com>
2010-09-07 05:48:16 +0000

    Strict upper bounds replaced with maximal path lengths, and the definition of the paths clarified.

--- include/program/jobs/Compute_Morse_Decomposition.h
+++ include/program/jobs/Compute_Morse_Decomposition.h
@@ -9,14 +9,14 @@
 /// using the SCC algorithm. The Conley Morse graph object
 /// given in the input must be initially empty, and is filled in
 /// the associated data structures with all the necessary information:
-/// strict upper bounds for the lengths of paths that begin at the
-/// Morse sets and exit the domain,
-/// strict upper bounds for the lengths of paths that enter the domain
-/// and then enter the Morse sets,
-/// strict upper bounds for the connecting path lengths between the
-/// Morse sets,
-/// and a strict upper bound for the length of a path that enters
-/// the domain, doesn't go through any Morse set, and leaves the domain.
+/// the maximal lengths of paths that begin at the Morse sets and end
+/// in any of the vertices which have an outgoing edge from the domain;
+/// the maximal lengths of paths that enter the Morse sets and begin
+/// at any vertex that has an icoming edge from outside the domain;
+/// the maximal lengtsh of paths connecting pairs of Morse sets;
+/// and the maximal length of a path that begins at any vertex
+/// with an edge incoming from outside the domain and ends at any vertex
+/// that has an edge outgoing from the domain.
 /// The provided vectors must be initially empty.
 template < class Toplex , class Conley_Morse_Graph , class Combinatorial_Map >
 void Compute_Morse_Decomposition ( Conley_Morse_Graph * conley_morse_graph ,
--- include/program/jobs/Compute_Path_Bounds.h
+++ include/program/jobs/Compute_Path_Bounds.h
@@ -8,8 +8,11 @@
 #include <map>
 
 
-/// Computes strict upper bounds on the lengths of connecting orbits
+/// Computes the maximal lenghts of connecting orbits
 /// between the Morse sets in the final Morse decomposition.
+/// The length of an orbit is the number of edges in a path
+/// whose first vertex is in the first Morse set
+/// and the last vertex is in the other Morse set.
 /// The provided data structure for path bounds is initially empty.
 /// The provided Conley-Morse graph contains all the edges for which
 /// there is a chance of the existence of a connection (in other words,
@@ -20,10 +23,10 @@
 /// and the corresponding Morse set is also indicated.
 /// For each Morse set, its decomposition is provided whenever the Morse set
 /// was actually subdivided.
-/// For each Morse set, the coarser Conley-Morse graph is provided
+/// For each Morse set, the coarser Conley-Morse graph is provided,
 /// as well as the corresponding set, if the Morse set is in a subdivision
 /// of some other Morse set.
-/// The bounds for path lengths, computed with graph algorithms, are also provided.
+/// The maximal path lengths, computed with graph algorithms, are also provided.
 template < class Conley_Morse_Graph >
 void Compute_Path_Bounds ( std::map < typename Conley_Morse_Graph::Edge , long > * path_bounds ,
   const Conley_Morse_Graph & conley_morse_graph ,
