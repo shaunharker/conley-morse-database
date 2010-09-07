28c138eaa1cca9471655e3c83f77d489ceebad33
Pawel Pilarczyk <paw.pilar@gmail.com>
2010-09-07 09:23:40 +0000

    A correction to the semantics of computed path lengths.

--- include/program/jobs/Compute_Morse_Decomposition.h
+++ include/program/jobs/Compute_Morse_Decomposition.h
@@ -5,19 +5,22 @@
 #ifndef _CMDP_COMPUTE_MORSE_DECOMPOSITION_
 #define _CMDP_COMPUTE_MORSE_DECOMPOSITION_
 
-/// Computes the Morse decomposition of a given set
-/// using the SCC algorithm. The Conley Morse graph object
-/// given in the input must be initially empty, and is filled in
-/// the associated data structures with all the necessary information:
-/// the maximal lengths of paths that begin at the Morse sets and end
-/// in any of the vertices which have an outgoing edge from the domain;
-/// the maximal lengths of paths that enter the Morse sets and begin
-/// at any vertex that has an icoming edge from outside the domain;
-/// the maximal lengtsh of paths connecting pairs of Morse sets;
-/// and the maximal length of a path that begins at any vertex
-/// with an edge incoming from outside the domain and ends at any vertex
-/// that has an edge outgoing from the domain.
+/// Computes the Morse decomposition of a given set using the SCC algorithm.
+/// The Conley Morse graph object given in the input must be empty.
+/// For each Morse set, computes the least strict upper bound for the length
+/// of a path that begins in the Morse set and end at any of the vertices
+/// which have an outgoing edge from the domain ("exit path").
+/// For each Morse set, computes the least strict upper bound for the length
+/// of a path that ends in the Morse set and begins at any vertex that has
+/// an icoming edge from outside the domain ("entrance path").
+/// For each connection between two Morse sets, computes the maximal length
+/// of a path connecting them.
+/// It also computes the least strict upper bound for the length of a path
+/// that begins at any vertex with an edge incoming from outside the domain
+/// and ends at any vertex that has an edge outgoing from the domain.
 /// The provided vectors must be initially empty.
+/// Note: The length 0 means "no path." The length 1 means "the shortest
+/// path possible."
 template < class Toplex , class Conley_Morse_Graph , class Combinatorial_Map >
 void Compute_Morse_Decomposition ( Conley_Morse_Graph * conley_morse_graph ,
   std::map < typename Conley_Morse_Graph::Vertex , long > * exit_path_bounds ,
