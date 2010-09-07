2375e59a79def6ddd1434e2f7685375d37d68d45
Pawel Pilarczyk <paw.pilar@gmail.com>
2010-09-07 01:40:32 +0000

    Updated function headers.

--- include/program/jobs/Single_Box_Job.h
+++ include/program/jobs/Single_Box_Job.h
@@ -13,21 +13,22 @@
 /// The Conley-Morse graph is stored in the data structure provided
 /// (which must be initially empty), together with Morse sets
 /// for which memory is dynamically allocated.
-template < class Toplex_Template , class Parameter_Toplex_Template , class Map_Template , class Conley_Index , class Cached_Box_Information >
-void Compute_Conley_Morse_Graph (
-  ConleyMorseGraph < typename Toplex_Template::Toplex_Subset, Conley_Index > * conley_morse_graph ,
-  Toplex_Template * phase_space ,
-  const typename Toplex_Template::Geometric_Description & phase_space_box ,
+template < class Toplex , class Parameter_Toplex ,
+  class Map , class Conley_Index , class Cached_Box_Information >
+void Compute_Conley_Morse_Graph ( ConleyMorseGraph < typename Toplex::Toplex_Subset, Conley_Index > * conley_morse_graph ,
+  const typename Parameter_Toplex::Geometric_Description & parameter_box ,
+  Toplex * phase_space ,
+  const typename Toplex::Geometric_Description & phase_space_box ,
   int subdivisions ,
-  Cached_Box_Informatin * cached_box_information
-);
+  Cached_Box_Informatin * cached_box_information );
 
 /// Constructs the Conley-Morse decomposition for a single parameter box.
 /// Runs a complete single box job as one of the worker's tasks.
 /// Reads the computation parameters from the job message.
 /// Saves the results of the computation in the result message.
-template < class Toplex_Template, class Parameter_Toplex_Template , class Map_Template >
-void Single_Box_Job ( Message * result, const Message & job ); 
+template < class Toplex, class Parameter_Toplex ,
+  class Map , class Conley_Index , class Cached_Box_Information >
+void Single_Box_Job ( Message * result, const Message & job );
 
 #ifndef _DO_NOT_INCLUDE_HPP_
 #include "program/jobs/Single_Box_Job.hpp"
--- include/program/jobs/Single_Box_Job.hpp
+++ include/program/jobs/Single_Box_Job.hpp
@@ -5,43 +5,60 @@
 #ifndef _CMDP_SINGLE_BOX_JOB_HPP_
 #define _CMDP_SINGLE_BOX_JOB_HPP_
 
+#include <map>
+
 #include "data_structures/Directed_Graph.h"
 #include "data_structures/Conley_Morse_Graph.h"
 #include "algorithms/Conley_Index.h"
 
 
 /// Computes the Morse decomposition of a given set
-/// using the SCC algorithm.
-template < class Toplex_Template , class Conley_Morse_Graph , class Combinatorial_Map >
-void Compute_Morse_Decomposition (
-  Conley_Morse_Graph * conley_morse_graph ,
-  const typename Toplex_Template::Toplex_Subset & morse_set ,
-  const Combinatorial_Map & combinatorial_map
-)
-{
-  // not yet implemented
+/// using the SCC algorithm. The Conley Morse graph object
+/// given in the input must be initially empty, and is filled in
+/// the associated data structures with all the necessary information:
+/// strict upper bounds for the lengths of paths that begin at the
+/// Morse sets and exit the domain,
+/// strict upper bounds for the lengths of paths that enter the domain
+/// and then enter the Morse sets,
+/// and strict upper bounds for the connecting path lengths.
+/// The provided vectors must be initially empty.
+template < class Toplex , class Conley_Morse_Graph , class Combinatorial_Map >
+void Compute_Morse_Decomposition ( Conley_Morse_Graph * conley_morse_graph ,
+  std::map < typename Conley_Morse_Graph::Vertex , long > * exit_path_bounds ,
+  std::map < typename Conley_Morse_Graph::Vertex , long > * entrance_path_bounds ,
+  std::map < typename Conley_Morse_Graph::Edge , long > * path_bounds ,
+  const typename Toplex::Toplex_Subset & domain ,
+  const Combinatorial_Map & combinatorial_map ) {
+  // not yet implemented: Zin
   return;
 } /* Compute_Morse_Decomposition */
 
 
-template < class Toplex_Template , class Parameter_Toplex_Template , class Map_Template , class Conley_Index , class Cached_Box_Information >
-void Compute_Conley_Morse_Graph (
-  ConleyMorseGraph < typename Toplex_Template::Toplex_Subset, Conley_Index > * conley_morse_graph ,
-  const typename Parameter_Toplex_Template::Geometric_Description & parameter_box ,
-  Toplex_Template * phase_space ,
-  const typename Toplex_Template::Geometric_Description & phase_space_box ,
+/// Computes a strict upper bound on the length of a connecting orbit
+/// between the Morse sets.
+template < class Conley_Morse_Graph >
+void Compute_Path_Bounds ( std::map < typename Conley_Morse_Graph::Edge , long > * path_bounds // UNDER CONSTRUCTION
+) {
+  // not yet implemented: Masaki
+} /* Compute_Path_Bounds */
+
+
+template < class Toplex , class Parameter_Toplex ,
+  class Map , class Conley_Index , class Cached_Box_Information >
+void Compute_Conley_Morse_Graph ( ConleyMorseGraph < typename Toplex::Toplex_Subset, Conley_Index > * conley_morse_graph ,
+  const typename Parameter_Toplex::Geometric_Description & parameter_box ,
+  Toplex * phase_space ,
+  const typename Toplex::Geometric_Description & phase_space_box ,
   int subdivisions ,
-  Cached_Box_Informatin * cached_box_information
-)
-{
+  Cached_Box_Informatin * cached_box_information ) {
   // short names for the types used in this function
-  typedef typename Toplex_Template::Toplex_Subset Toplex_Subset;
-  typedef ConleyMorseGraph < typename Toplex_Template::Toplex_Subset, Conley_Index > Conley_Morse_Graph;
+  typedef typename Toplex::Toplex_Subset Toplex_Subset;
+  typedef ConleyMorseGraph < typename Toplex::Toplex_Subset, Conley_Index > Conley_Morse_Graph;
   typedef std::vector < Conley_Morse_Graph > Conley_Morse_Graphs;
 
   // create the objects of the maps
-  Map_Template interval_map ( parameter_box );
-  Combinatorial_Map < Toplex_Template , Map_Template > combinatorial_map ( phase_space , interval_map );
+  Map interval_map ( parameter_box );
+  Combinatorial_Map < Toplex , Map > combinatorial_map ( phase_space , interval_map );
 
   // create the initial (trivial) Morse decomposition of the entire phase space
   conley_morse_graph -> AddVertex ();
@@ -89,13 +106,14 @@ void Compute_Conley_Morse_Graph (
 } /* Compute_Conley_Morse_Graph */
 
 
-template < class Toplex_Template, class Parameter_Toplex_Template , class Map_Template >
+template < class Toplex, class Parameter_Toplex ,
+  class Map , class Conley_Index , class Cached_Box_Information >
 void Single_Box_Job ( Message * result, const Message & job ) {
   // short names for the types used in this function
-  typedef std::vector < typename Toplex_Template::Toplex_Subset > Morse_Sets;
-  typedef typename Toplex_Template::Geometric_Description Geometric_Description;
-  typedef typename Parameter_Toplex_Template::Geometric_Description Parameter_Geometric_Description;
-  typedef ConleyMorseGraph < typename Toplex_Template::Toplex_Subset, Conley_Index > Conley_Morse_Graph;
+  typedef std::vector < typename Toplex::Toplex_Subset > Morse_Sets;
+  typedef typename Toplex::Geometric_Description Geometric_Description;
+  typedef typename Parameter_Toplex::Geometric_Description Parameter_Geometric_Description;
+  typedef ConleyMorseGraph < typename Toplex::Toplex_Subset, Conley_Index > Conley_Morse_Graph;
 
   // read the job parameters from the input message
   job . open_for_reading ();
@@ -107,10 +125,12 @@ void Single_Box_Job ( Message * result, const Message & job ) {
   job >> phase_space_box;
   int subdivisions ( 0 );
   job >> subdivisions;
+  Cached_Box_Informatin cached_box_information;
+  job >> cached_box_information;
   job . close ();
 
   // create the object of the toplex for the given phase space box
-  Toplex_Template phase_space ( phase_space_box );
+  Toplex phase_space ( phase_space_box );
 
   // a collection of Morse sets will be stored in an std::vector;
   // note that this might be inefficient because of copying large objects
@@ -118,12 +138,15 @@ void Single_Box_Job ( Message * result, const Message & job ) {
 
   // compute the Conley-Morse graph
   Conley_Morse_Graph conley_morse_graph;
-  Compute_Conley_Morse_Graph ( & conley_morse_graph , parameter_box , & phase_space , phase_space_box , subdivisions );
+  Compute_Conley_Morse_Graph < Toplex , Parameter_Toplex , Map , Conley_Index , Cached_Box_Information> (
+    & conley_morse_graph , parameter_box , & phase_space , phase_space_box ,
+    subdivisions , & cached_box_information );
 
   // prepare the message with the result of the computations
   result -> open_for_writing ();
   *result << job_number;
   *result << conley_morse_graph;
+  *result << cached_box_information;
   result -> close ();
   return;
 } /* Single_Box_Job */
