1b29a2a9d553fccdec60002417dec880ecf8a4ef
Zin Arai <arai@cris.hokudai.ac.jp>
2010-09-07 04:48:08 +0000

    update the test digraph

--- test/Test_Directed_Graph.cpp
+++ test/Test_Directed_Graph.cpp
@@ -4,43 +4,67 @@
 
 struct Toplex 
 {
-  typedef int Top_Cell;
+  typedef unsigned int size_type;
+  typedef size_type Top_Cell;
   typedef std::set<Top_Cell> Subset;
 };
 
 int main ( int argc, char * argv [] ) 
 {
-  typedef DirectedGraph <Toplex> MyGraph;
+  typedef DirectedGraph <Toplex> Graph;
 
-  int nVertices = 3;
+  int nVertices = 10;
   Toplex::Subset E[nVertices];
 
   /*
-    The sample graph G has 3 vertices with edges defined by
-    0 |--> 0, 1
+    The sample graph G has 10 vertices with edges defined by
+    0 |--> 1
     1 |--> 2
-    2 |--> 2
-    Therefore, there should be two compones of scc(G), namely, {0} and {2}  
+    2 |--> 3, 7
+    3 |--> 4
+    4 |--> 1
+    5 |--> 5, 6
+    6 |-->
+    7 |--> 8, 9
+    8 |--> 9
+    9 |--> 7
+    Therefore, there should be three compones: {1234}, {5} and {789}  
    */
 
-  E[0].insert(0);
   E[0].insert(1);
   E[1].insert(2);
-  E[2].insert(2);
+  E[2].insert(3);
+  E[2].insert(7);
+  E[3].insert(4);
+  E[4].insert(1);
+  E[5].insert(5);
+  E[5].insert(6);
+  E[7].insert(8);
+  E[7].insert(9);
+  E[8].insert(9);
+  E[9].insert(7);
 
-  MyGraph G;
+  Graph G;
   for (int i = 0; i < nVertices; i++) {
-    G.insert(MyGraph::value_type(i, E[i]));
+    G.insert(Graph::value_type(i, E[i]));
   }
   std::cout << "The size of the Graph: " << G.size() << std::endl; 
 
   std::vector< Toplex::Subset > Components;
-
   G.getStronglyConnectedComponents(Components);
-  G.getConnectingPathLength(0, 0);
-  G.getStablePathLength(0);
-
   std::cout << "The number of SCCs: " << Components.size() << std::endl;
+
+  std::vector< Toplex::Subset >::iterator it = Components.begin();
+  while (it != Components.end()) {
+      std::cout << "component: ";
+      Toplex::Subset::iterator comp_it = (*it).begin();
+      while (comp_it != (*it).end()) {
+          std::cout << *comp_it;;
+          ++comp_it;
+      }
+      std::cout << std::endl;
+      ++it;
+  }
 
   return 0;
 }
