9a1b5165af939c874bcb8f0349e1b7a50b5c5fd1
Ippei Obayashi <obayashi@math.kyoto-u.ac.jp>
2010-09-07 02:46:25 +0000

    fix ClutchingTwoGraphs and add Union-Find structure

--- include/program/jobs/Clutching_Graph_Job.hpp
+++ include/program/jobs/Clutching_Graph_Job.hpp
@@ -5,6 +5,136 @@
 #include "data_structures/Conley_Morse_Graph.h"
 #include "distributed/Message.h"
 #include <boost/foreach.hpp>
+#include <algorithm>
+#include <vector>
+
+
+/** Quotient Set class.
+ *  Holds all equivalent classes using union-find structure.
+ *  T must be: copyable, assignable, default constructable, and comparable
+ */
+template<class T>
+class UnionFind {
+ public:
+  template<class Iterator>
+  UnionFind(Iterator begin, Iterator end);
+  
+  /** If x and y contain different sets, these two sets are unioned.
+   */
+  void Union(T x, T y);
+  /** Return true if x and y contain the same class.
+   */
+  bool Find(T x, T y) const;
+  /** Return the representative element of the set containing x */
+  T Representative(T x) const;
+
+  /** Merge other UnionFind */
+  void Merge(const UnionFind<T> &other);
+
+  /** Add a new element {x} to Quotient set. if x is already contained,
+   *  this function causes no effect.
+   */
+  void Add(T x);
+  
+  template<class Iterator>
+  void Add(Iterator begin, Iterator end);
+ private:
+  struct Entry {
+    T parent;
+    /* if rank == 0, this entry is not a root, otherwise, this entry is root and
+     * rank means the size of the set.
+     */
+    int rank; 
+  };
+  mutable std::map<T, Entry> tree_;
+};
+
+template<class T> void UnionFind<T>::Union(T x, T y) {
+  x = Representative(x);
+  y = Representative(y);
+  if (x != y) {
+    Entry *x_entry = &tree_[x];
+    Entry *y_entry = &tree_[y];
+    if (x_entry->rank < y_entry->rank)
+      std::swap(x_entry, y_entry);
+    x_entry->rank += y_entry->rank;
+    y_entry->parent = x;
+    y_entry->rank = 0;
+  }
+}
+
+template<class T>
+template<class Iterator>
+UnionFind<T>::UnionFind(Iterator begin, Iterator end) {
+  for (Iterator i=begin; i != end; i++) {
+    tree_[*i].rank = 1;
+  }
+}
+
+template<class T>
+bool UnionFind<T>::Find(T x, T y) const {
+  return Representative(x) == Representative(y);
+}
+
+template<class T>
+T UnionFind<T>::Representative(T x) const {
+  if (tree_[x].rank > 0) {
+    return x;
+  } else {
+    T root = Representative(tree_[x].parent);
+    tree_[x].parent = root;
+    return root;
+  }
+}
+
+template<class T>
+void UnionFind<T>::Add(T x) {
+  if (tree_.find(x) == tree_.end()) {
+    tree_[x].rank = 1;
+  }
+}
+
+template<class T>
+void UnionFind<T>::Merge(const UnionFind<T> &other) {
+  T key;
+  Entry entry;
+
+  BOOST_FOREACH (boost::tie(key, entry), other.tree_) {
+    T root = other.Representative(key);
+    Add(root);
+    Add(key);
+    Union(root, key);
+  }
+}
+
+template<class CMGraph>
+class VertexPairs {
+ public:
+  typedef typename CMGraph::Vertex Vertex;
+  typedef typename std::pair<Vertex, Vertex> Pair;
+  typedef typename std::vector<std::pair<Vertex, Vertex> >::const_iterator ConstIterator;
+  typedef typename std::vector<std::pair<Vertex, Vertex> >::iterator Iterator;
+
+  /* Create an emtpy set */
+  VertexPairs() {}
+  
+  /* Add a pair */
+  void Add(Vertex v1, Vertex v2) {
+    pairs_.push_back(std::pair<Vertex, Vertex>(v1, v2));
+  }
+
+  /* Return a pair of begin/end iterators */
+  std::pair<ConstIterator, ConstIterator> Pairs() const {
+    return std::pair<ConstIterator, ConstIterator>(pairs_.begin(), pairs_.end());
+  }
+
+  std::pair<Iterator, Iterator> Pairs() {
+    return std::pair<Iterator, Iterator>(pairs_.begin(), pairs_.end());
+  }
+  
+ private:
+  std::vector<std::pair<Vertex, Vertex> > pairs_;
+};
 
 
 /** Compute a relation between two C-M graph
@@ -17,8 +147,11 @@
 template<class CMGraph>
 bool ClutchingTwoGraphs(
     const CMGraph &graph1,
-    const CMGraph &graph2) {
+    const CMGraph &graph2,
+    VertexPairs<CMGraph> *pairs) {
   typedef typename CMGraph::Vertex Vertex;
+
+  std::set<Vertex> used;
   bool result = true;
 
   if (graph1.NumVertices() != graph2.NumVertices())
@@ -28,12 +161,21 @@ bool ClutchingTwoGraphs(
     int n = 0;
     BOOST_FOREACH (Vertex v2, graph2.Vertices()) {
       if (Check_If_Intersect(*graph1.GetCubeSet(v1), *graph2.GetCubeSet(v2))) {
+        if (pairs)
+          pairs->Add(v1, v2);
+        
         n++;
-        /* if two reccurent sets in one C-M graph intersect
+        /* if two components in one C-M graph intersect
            one reccurent set in other C-M graph, the two graphs does not
            share the "same" structure */
         if (n >= 2)
           result = false;
+        if (!used.insert(v2).second)
+          result = false;
+
+        /* if two intersected components have different properties,
+           the two graphs does not
+           share the "same" structure */
         result = result &&
                  (*graph1.GetConleyIndex(v1) == *graph2.GetConleyIndex(v2));
       }
--- test/Test_Clutching_Graph.cpp
+++ test/Test_Clutching_Graph.cpp
@@ -17,11 +17,27 @@ void SetupCMGraph(CMGraph *graph, std::vector<CMGraph::Vertex> *vertices,
   }
 }
 
+
+void ShowVertexPairs(const CMGraph &graph1, const int *ptr1,
+                     const CMGraph &graph2, const int *ptr2,
+                     const VertexPairs<CMGraph> &pairs)
+{
+  BOOST_FOREACH(VertexPairs<CMGraph>::Pair pair, pairs.Pairs()) {
+    std::cout << "(" << graph1.GetCubeSet(pair.first) - ptr1
+              << "," << graph2.GetCubeSet(pair.second) - ptr2
+              << ")" << " ";
+  }
+  std::cout << std::endl;
+}
+
 int main(int argc, char *argv[])
 {
   CMGraph cmgraph1;
+  typedef VertexPairs<CMGraph> Pairs;
+  
   int toplexes1[4] = { 0, 1, 2, 3 };
   int indices1[4] = { 0, 1, 2, 3};
+  
   std::vector<CMGraph::Vertex> vs1;
   SetupCMGraph(&cmgraph1, &vs1, 4, toplexes1, indices1);
 
@@ -38,15 +54,58 @@ int main(int argc, char *argv[])
   SetupCMGraph(&cmgraph3, &vs3, 5, toplexes3, indices3);
 
   CMGraph cmgraph4;
-  int toplexes4[4] = { 0, 1, 2, 1};
-  int indices4[4] = { 0, 1, 2, 1};
+  int toplexes4[4] = { 3, 0, 1, 2};
+  int indices4[4] = { 3, 0, 1, 2};
   std::vector<CMGraph::Vertex> vs4;
   SetupCMGraph(&cmgraph4, &vs4, 4, toplexes4, indices4);
+
+  /* true is assumed, because two graph is the same  */
+  VertexPairs<CMGraph> pairs11;
+  std::cout << ClutchingTwoGraphs(cmgraph1, cmgraph1, &pairs11) << std::endl;
+  ShowVertexPairs(cmgraph1, toplexes1, cmgraph1, toplexes1, pairs11);
+
+  /* false is assumed */
+  VertexPairs<CMGraph> pairs12;
+  std::cout << ClutchingTwoGraphs(cmgraph1, cmgraph2, &pairs12) << std::endl;
+  ShowVertexPairs(cmgraph1, toplexes1, cmgraph2, toplexes2, pairs12);
+
+  /* false is assumed, because cmgraph3 is larger than cmgraph1 */
+  VertexPairs<CMGraph> pairs13;
+  std::cout << ClutchingTwoGraphs(cmgraph1, cmgraph3, &pairs13) << std::endl;
+  ShowVertexPairs(cmgraph1, toplexes1, cmgraph3, toplexes3, pairs13);
+
+  /* true is assumed, because cmgraph4 is shuffle of  cmgraph1 */
+  VertexPairs<CMGraph> pairs14;
+  std::cout << ClutchingTwoGraphs(cmgraph1, cmgraph4, &pairs14) << std::endl;
+  ShowVertexPairs(cmgraph1, toplexes1, cmgraph4, toplexes4, pairs14);
+
+  /* false is assumed */
+  VertexPairs<CMGraph> pairs24;
+  std::cout << ClutchingTwoGraphs(cmgraph2, cmgraph4, &pairs24) << std::endl;
+  ShowVertexPairs(cmgraph2, toplexes2, cmgraph4, toplexes4, pairs24);
   
-  std::cout << ClutchingTwoGraphs(cmgraph1, cmgraph1) << std::endl;
-  std::cout << ClutchingTwoGraphs(cmgraph1, cmgraph2) << std::endl;
-  std::cout << ClutchingTwoGraphs(cmgraph1, cmgraph3) << std::endl;
-  std::cout << ClutchingTwoGraphs(cmgraph2, cmgraph4) << std::endl;
+  std::cout << "--" << std::endl;
+  
+  int data[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+  UnionFind<int> u(data, data+10);
+  int data2[6] = {1, 8, 12, 13, 14, 15};
+  UnionFind<int> v(data2, data2+6);
+  
+  u.Union(1, 2);
+  u.Union(3, 4);
+  std::cout << u.Find(1, 2) << std::endl;
+  std::cout << u.Find(1, 3) << std::endl;
+  u.Union(1, 4);
+  std::cout << u.Find(1, 3) << std::endl;
+  std::cout << u.Find(5, 6) << std::endl;
+
+  v.Union(1, 8);
+  v.Union(12, 15);
+
+  u.Merge(v);
+  std::cout << u.Find(2, 8) << std::endl;
+  std::cout << u.Find(12, 15) << std::endl;
+  std::cout << u.Find(12, 14) << std::endl;
   
   return 0;
 }
