a97f5e402043b9b7cce201b0a0fb2d2412afaabf
Shaun Harker <sharker81@gmail.com>
2010-09-08 07:03:42 +0000

    marcio's coordinator

--- include/program/Coordinator.h
+++ include/program/Coordinator.h
@@ -2,20 +2,34 @@
  *  Coordinator.h
  */
 
-
 #ifndef _CMDP_COORDINATOR_
 #define _CMDP_COORDINATOR_
 
+#include "toplexes/Adaptive_Cubical_Toplex.h"
+#include "complexes/Adaptive_Complex.h"
+#include "algorithms/basic.h"
 #include "distributed/Distributed.h"
+#include "data_structures/Cached_Box_Information.h"
 
-/* Sample Coordinator class */
+using namespace Adaptive_Cubical;
+
+typedef Toplex Parameter_Toplex;
+typedef std::map <Toplex::Top_Cell, Cached_Box_Information> Cached_Box_Map;
+typedef std::pair <Toplex::Top_Cell, Cached_Box_Information> Cached_Box_Pair;
+typedef unsigned int size_type;
+
+/// Coordinator class
 class Coordinator : CoordinatorBase {
  public:
   Coordinator(int argc, char **argv);
   virtual State Prepare(Message *job);
   virtual void Process(const Message &result);
  private:
-  int n_, done_;
+  typedef unsigned int size_type;
+  size_type num_jobs_;
+  size_type num_jobs_sent_;
+  size_type num_jobs_received_;
+  std::vector < Toplex_Subset > patches_;
 };
 
 #ifndef _DO_NOT_INCLUDE_HPP_
