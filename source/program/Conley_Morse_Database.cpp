6d00b6fc9a8a8af9ce66561e9aaa658501723891
Shaun Harker <sharker81@gmail.com>
2011-09-03 07:11:00 +0000

    configuration accepts NO_CONLEY_INDEX_IN_DATABASE define

--- source/program/Conley_Morse_Database.cpp
+++ source/program/Conley_Morse_Database.cpp
@@ -5,11 +5,14 @@
 #include "delegator/delegator.h"  /* For Coordinator_Worker_Scheme<>() */
 #include "program/MorseProcess.h"
 #include "program/ConleyProcess.h"
+#include "program/Configuration.h"
 
 int main ( int argc, char * argv [] ) {
   delegator::Start ();
 	delegator::Run < MorseProcess > (argc, argv); 
-	delegator::Run < ConleyProcess > (argc, argv); 
+#ifndef NO_CONLEY_INDEX_IN_DATABASE
+	delegator::Run < ConleyProcess > (argc, argv);
+#endif	 
 	delegator::Stop ();
 	return 0;
 }
