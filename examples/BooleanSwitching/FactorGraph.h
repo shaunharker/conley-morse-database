f2ea645ecb4a45f6569590b7f2cfca340c3f0f75
Shaun Harker <sharker81@gmail.com>
2014-05-09 05:42:00 +0000

    Working on BooleanSwitching example

---
+++ examples/BooleanSwitching/FactorGraph.h
@@ -0,0 +1,302 @@
+
+#ifndef BOOLEANSWITCHINGFACTORGRAPH_H
+#define BOOLEANSWITCHINGFACTORGRAPH_H
+
+/// class ConnectedSmartGraph where vertices are smart in the sense that they
+///  (a) supply neighbors via a method "neighbors"
+///  (b) the graph is connected
+template < class T >
+class ConnectedSmartGraph {
+public:
+  // gives indexing
+  boost::unordered_map<T, int> preorder;
+  // array of monotonic functions
+  std::vector<T> vertices;
+
+  // optional
+  std::vector<std::vector< int > > adjacencies_;
+
+  void construct ( const T & start ) {
+    vertices . push_back ( start );
+    preorder [ start ] = 0;
+
+    std::stack<T> dfs_stack;
+    dfs_stack . push ( start );
+
+    size_t count = 0;
+    while ( not dfs_stack . empty () ) {
+      std::cout << ++ count << "   and stack size is " << dfs_stack . size () << "\n";
+      T vertex = dfs_stack . top ();
+      dfs_stack . pop ();
+      std::vector<boost::shared_ptr<T> > neighbors = vertex . neighbors ();
+      //std::cout << "Found " << neighbors . size () << " neighbors.\n";
+      BOOST_FOREACH ( boost::shared_ptr<T> ptr, neighbors ) {
+        if ( preorder . count ( *ptr ) ) continue;
+        preorder [ * ptr ] = vertices . size ();
+        vertices . push_back ( * ptr );
+        dfs_stack . push ( * ptr );
+      }
+    }
+
+    std::cout << "Number of unique monotonic maps = " << vertices . size () << "\n";
+
+  }
+
+  void compute_adjacencies ( void ) {
+    adjacencies_ . resize ( vertices . size () );
+    for ( int v = 0; v < vertices . size (); ++ v ) {
+      T vertex = vertices [ v ];
+      std::vector<boost::shared_ptr<T> > neighbors = vertex . neighbors ();
+      BOOST_FOREACH ( boost::shared_ptr<T> ptr, neighbors ) {
+        adjacencies_ [ v ] . push_back ( preorder [ *ptr ] );
+      }
+    }
+  }
+
+  const std::vector<int> & adjacencies ( int v ) const {
+    return adjacencies_ [ v ];
+  }
+
+  void saveToFile ( void ) const {
+    std::ofstream outfile ( "graph.gv" );
+    outfile << "graph factorgraph {\n";
+    for ( int v = 0; v < vertices . size (); ++ v ) {
+      const T & vertex = vertices [ v ];
+      std::vector<boost::shared_ptr<T> > neighbors = vertex . neighbors ();
+      BOOST_FOREACH ( boost::shared_ptr<T> ptr, neighbors ) {
+        int u = preorder . find ( * ptr) -> second;
+        if ( v < u ) {
+          outfile << v << " -- " << u << "\n";
+        }
+      }
+    }
+    outfile << "}\n\n";
+  }
+
+};
+
+/// class MonotonicMap
+/// a "smart vertex" class representing the dynamics of a node of
+/// a boolean switching network
+class MonotonicMap {
+public:
+  // data
+  int n; // number of in edges  -- domain is {0,1,...,2^n-1}
+  int m; // number of out edges -- codomain is {0,1,...,m}
+
+  /// data_
+  ///    data_ is an array storing the value of
+  ///    a map from {0,1,...2^n-1} -> {0,1,...m}
+  std::vector< int > data_;  // mapping
+
+  /// logic_
+  ///    An expression of the form (a+b+c)(d+e)f(g+h)
+  ///    would be encoded as vector of ints 3,2,1,2 (the number of summands in each
+  ///    factor). Up-regulation and down-regulation are of no concern here (they are
+  ///    treated elsewhere)
+  ///    The entries in logic_ line up to the bits in the domain of data_
+  ///    in such a way that the last entry of logic_ corresponds to the least
+  ///    significant bit of data_'s domain.
+  std::vector< int> logic_; // logic
+  // constructors
+  MonotonicMap ( void ) {}
+  MonotonicMap ( int n, int m ) : n(n), m(m) {
+    data_ . resize ( (1 << n), 0 );
+    logic_ . resize ( 1, n ); // all-sum by default
+  }
+  MonotonicMap ( int n, int m, const std::vector<int> & logic ): n(n), m(m), logic_(logic) {
+    data_ . resize ( (1 << n), 0 );
+  }
+  MonotonicMap ( int n, 
+                 int m, 
+                 const std::vector<int> & logic, 
+                 const std::vector<int> & data )
+    : n(n), m(m), logic_(logic), data_(data) {}
+
+  // Check if monotonic
+  bool monotonic ( void ) const {
+    //std::cout << "Calling monotonic\n";
+    int N = (1 << n);
+    for ( int i = 0; i < N; ++ i ) {
+      std::vector<int> children;
+      for ( int pos = 0; pos < n; ++ pos ) {
+        int bit = 1 << pos;
+        if ( not ( i & bit ) ) { 
+          //std::cout << "Pushing child " << (i|bit) << " of " << i << "\n";
+          children . push_back ( i | bit );
+        }
+      }
+      BOOST_FOREACH ( int child, children ) {
+        if ( data_[child] < data_[i] ) { 
+          //std::cout << " Return false because data_[" << child << "] < data_[" << i << "]\n";
+          //std::cout << " data_[" << child << "] = " << data_[child] << "\n";
+          //std::cout << " data_[" << i << "] = " << data_[i] << "\n";
+
+          return false;
+        }
+      }
+    }
+    //std::cout << "Returning true.\n";
+    return true;
+  }
+
+  bool realizable ( void ) const {
+    // currently this is sum-sum realizable
+
+    // What is realizable?
+    // For each subset of variables I,
+    // define the complement to be J,
+    // We want to realize a partial order on 2^I
+    // by inducing it via every substitution of selection
+    // of J variables.
+    // This means I have to loop through all choice I of subsets of variables.
+    // That shouldn't be hard. Then I have to loop through 
+    // substitutions of the J variables.
+    // And within that, I need to loop through pairs of choices of substitutions for
+    // the I variables.
+    // An upper bound for this procedure is 8^n. For small n it should be tractable.
+    // Actually it would be better to loop through the pairs first and the
+    // substitutions second, to avoid having to store information
+    int max_terms_in_factor = 0;
+    for ( int i = 0; i < logic_ . size (); ++ i ) {
+      max_terms_in_factor = std::max ( max_terms_in_factor, logic_[i] );
+    }
+
+    if ( (logic_ . size () == 1) || (max_terms_in_factor == 1) ) {
+      // Case (n) (all sum case) or Case (1,1,1,1...,1) (n-times, all product case)
+      int N = (1 << n);
+      for ( int i = 0; i < N; ++ i ) {
+        // I corresponds to the bits of i that are on.
+        //std::cout << "Top of loop\n";
+
+        for ( int a = 0; a < N; ++ a ) {
+          if ( (a & i) != a ) continue;
+          for ( int b = 0; b < N; ++ b ) {
+            if ( (b & i) != b ) continue;
+            bool less = false;
+            bool greater = false;
+            for ( int c = 0; c < N; ++ c ) {
+              if ( (c & i ) != 0 ) continue;
+              int x = data_[a|c];
+              int y = data_[b|c];
+              /*
+              std::cout << "\n a = " << a << "\n";
+              std::cout << " b = " << b << "\n";
+              std::cout << " c = " << c << "\n";
+              std::cout << " a|c = " << (a|c) << "\n";
+              std::cout << " b|c = " << (b|c) << "\n";
+              std::cout << " data_[a|c] = " << data_[a|c] << "\n";
+              std::cout << " data_[b|c] = " << data_[b|c] << "\n";
+              */
+
+              if ( x < y ) { 
+                less = true;
+              }
+              if ( x > y ) { 
+                greater = true;
+              }
+              if ( less && greater ) {
+                //std::cout << "Returning false.\n";
+                return false;
+              }
+            }
+          }
+        }
+      }
+      return true;
+    } else if ( logic_ . size () == 2 ) {
+      if ( logic_[0] == 2 && logic_[1] == 1 ) {
+        // Case (2, 1)
+        // In this notation the (correct) rules I gave for sum-product amount to
+        // " 010 < 001 implies 110 <= 101 "  (Rule A)
+        // " 100 < 001 implies 110 <= 011 "  (Rule B)
+        // and then we have two versions of Rule C, which does allow a reverse:
+        // " 010 > 100 implies 011 >= 101 "  (Rule C)
+        // " 010 < 100 implies 011 <= 101 "  (Rule C, reversed)
+        int D010 = data_[2];
+        int D001 = data_[1];
+        int D110 = data_[6]; 
+        int D101 = data_[5];
+        int D100 = data_[4];
+        int D011 = data_[3];
+
+        if ( (D010 < D001) && not (D110 <= D101) ) return false;
+        if ( (D100 < D001) && not (D110 <= D011) ) return false;
+        if ( (D010 > D100) && not (D011 >= D101) ) return false;
+        if ( (D010 < D100) && not (D011 <= D101) ) return false;
+        return true;
+      }
+      if ( logic_[0] == 1 && logic_[1] == 2 ) {
+        // Case (1,2). Symmetric to case (2,1). (We just rotate the bits)
+        int D010 = data_[2];
+        int D001 = data_[1];
+        int D110 = data_[6]; 
+        int D101 = data_[5];
+        int D100 = data_[4];
+        int D011 = data_[3];
+
+        if ( (D001 < D100) && not (D011 <= D110) ) return false;
+        if ( (D010 < D100) && not (D011 <= D101) ) return false;
+        if ( (D001 > D010) && not (D101 >= D110) ) return false;
+        if ( (D001 < D010) && not (D101 <= D110) ) return false;
+        return true;     
+      }
+    } 
+    std::cout << "BooleanSwitching Node realizability condition unknown.\n";
+    throw std::logic_error ( "Incomplete algorithm\n");
+    return false;
+  }
+
+  // return adjacent monotonic maps
+  std::vector<boost::shared_ptr<MonotonicMap> > neighbors ( void ) const {
+    //std::cout << "Calling neighbors.\n";
+    std::vector<boost::shared_ptr<MonotonicMap> > results;
+
+    // Obtain neighbors via changing the monotone function
+    std::vector<int> copy = data_;
+    int N = (1 << n);
+    for ( int i = 0; i < N; ++ i ) {
+      if ( copy[i] > 0 ) {
+        -- copy[i];
+        boost::shared_ptr<MonotonicMap> new_map ( new MonotonicMap ( n, m, copy, logic_ ) );
+        if ( new_map -> monotonic () && new_map -> realizable () ) 
+          results . push_back ( new_map );
+        ++ copy[i];
+      }
+      if ( copy[i] < m ) {
+        ++ copy[i];
+        boost::shared_ptr<MonotonicMap> new_map ( new MonotonicMap ( n, m, copy, logic_ ) );
+        if ( new_map -> monotonic () && new_map -> realizable () ) 
+          results . push_back ( new_map );
+        -- copy[i];
+      }
+    }
+
+    return results;
+  }
+
+  bool operator == ( const MonotonicMap & rhs ) const {
+    if ( n != rhs . n ) return false;
+    if ( m != rhs . m ) return false;
+    int N = (1 << n);
+    for ( int i = 0; i < N; ++ i ) {
+      if ( data_[i] != rhs.data_[i] ) return false;
+    }
+    return true;
+  }
+
+  friend std::size_t hash_value ( const MonotonicMap & p ) {
+    std::size_t seed = 0;
+    int N = (1 << p.n);
+    for ( int i = 0; i < N; ++ i ) {
+      boost::hash_combine(seed, p.data_[i] );
+    }
+    return seed;
+  }
+};
+
+// Specialization of ConnectedSmartGraph to MonotonicMap smart vertices
+typedef ConnectedSmartGraph<MonotonicMap> FactorGraph;
+
+#endif
+
--- examples/BooleanSwitching/Network.h
+++ examples/BooleanSwitching/Network.h
@@ -1,26 +1,177 @@
-#ifndef NETWORK_H
-#define NETWORK_H
+#ifndef BOOLEANSWITCHINGNETWORK_H
+#define BOOLEANSWITCHINGNETWORK_H
 
+// char_sep_example_2.cpp
+#include <iostream>
+#include <fstream>
+#include <boost/tokenizer.hpp>
+#include <string>
 #include <vector>
+#include <exception>
+#include "boost/unordered_map.hpp"
+#include "boost/foreach.hpp"
 
-// A class representing a local piece of a regulatory network
-class Network {
-public:
-	int numVariables;
-	// An array storing which nodes feed-in information
-	std::vector<std::vector<int> > inputs;
-	// An array storing which feed-in nodes are up regulators (true)
-	// and which are down-regulators (false)
-	std::vector< std::vector<bool> > regulation;
-	// An array storing which nodes use information
-	std::vector<std::vector<int> > outputs;
-	// An array storing how many thresholds there are per variable 
-	// (which implies there will be one more interval, however:
-	//   (0, t_1) (t_1,t_2) ... (t_{n-1}, t_n) (t_n, \infty) )
-	std::vector< int > threshold_count;
-private:
+namespace BooleanSwitching {
 
+struct NodeData {
+  int index;
+  std::vector<std::vector<int> > logic;
+  std::vector<int> out_order;
 };
 
+class Network {
+public:
+  boost::unordered_map<std::string, int> nodes_;
+  std::vector<NodeData> node_data_;
+};
+
+inline std::ostream & 
+operator << ( std::ostream & stream, 
+              const NodeData & data ) {
+  stream << "Node " << data . index << ":\n";
+  BOOST_FOREACH ( const std::vector<int> & factor, data . logic ) {
+    stream << "(";
+    for ( int i = 0; i < factor . size (); ++ i ) {
+      if ( i != 0 ) stream << ", ";
+      stream << factor [ i ];
+    }
+    stream << ")";
+  }
+  stream << "\n  Out Order:  ";
+  for ( int i = 0; i < data . out_order . size (); ++ i ) {
+    if ( i != 0 ) stream << ", ";
+    stream << data . out_order [ i ];
+  }
+  stream << "\n";
+  return stream;
+}
+
+
+
+inline std::ostream & 
+operator << ( std::ostream & stream, 
+              const Network & network ) {
+  stream << "There are " << network . nodes_ . size () << " nodes with the following names and indices:\n";
+  typedef std::pair<const std::string, int> value_t;
+  BOOST_FOREACH ( const value_t & node, network . nodes_ ) {
+    stream << node . first << " <-> " << node . second << "\n";
+  }
+  BOOST_FOREACH ( const NodeData & data, network . node_data_ ) {
+    stream << data << "\n";
+  }
+  return stream;
+}
+
+
+inline int 
+parseNodeName ( const std::string & field, 
+                boost::unordered_map<std::string, int> & nodes_ ) {
+  //std::cout << "NODE: " << field << "\n";
+  typedef boost::tokenizer<boost::char_separator<char> > tokenizer;
+  boost::char_separator<char> sep(" ", "~");
+  tokenizer tokens(field, sep);
+  tokenizer::iterator tok_iter = tokens . begin ();
+  if ( tok_iter == tokens . end () ) throw std::logic_error ( field );
+  std::string token = * tok_iter ++;
+  std::string nodeName;
+  bool negated;
+  if ( token == std::string ( "~" ) ) { 
+    negated = true;
+    if ( tok_iter == tokens . end () ) throw std::logic_error ( field );
+    nodeName = * tok_iter ++;
+  } else {
+    negated = false;
+    nodeName = token;
+  }
+  if ( nodes_ . count ( nodeName ) == 0 ) {
+    int new_index = nodes_ . size () + 1;
+    nodes_ [ nodeName ] = new_index;
+  } 
+  if ( tok_iter != tokens . end () ) throw std::logic_error ( field );
+  return ( negated ? -1 : 1 ) * nodes_ [ nodeName ];
+}
+
+inline std::vector<int> 
+parseSum( const std::string & field,
+          boost::unordered_map<std::string, int> & nodes_ ) {
+  //std::cout << "SUM: " << field << "\n";
+  std::vector<int> result;
+  typedef boost::tokenizer<boost::char_separator<char> > tokenizer;
+  boost::char_separator<char> sep("+ ");
+  tokenizer tokens(field, sep);
+  BOOST_FOREACH ( std::string summand, tokens ) {
+    result . push_back ( parseNodeName ( summand, nodes_ ) );
+  }
+  return result;
+}
+
+inline std::vector< std::vector<int> > 
+parseProductOfSums( const std::string & field,
+                    boost::unordered_map<std::string, int> & nodes_ ) {
+  //std::cout << "POS: " << field << "\n";
+  std::vector< std::vector<int> > result;
+  typedef boost::tokenizer<boost::char_separator<char> > tokenizer;
+  boost::char_separator<char> sep("()");
+  tokenizer tokens(field, sep);
+  BOOST_FOREACH ( std::string factor, tokens ) {
+    std::vector<int> factor_parse = parseSum ( factor, nodes_ );
+    if ( factor_parse . size () > 0 ) result . push_back ( factor_parse );
+  }
+  return result;
+}
+
+inline std::vector< int > 
+parseOutOrder( const std::string & field,
+               boost::unordered_map<std::string, int> & nodes_ ) {
+  //std::cout << "OUT: " << field << "\n";
+  std::vector<int> result;
+  typedef boost::tokenizer<boost::char_separator<char> > tokenizer;
+  boost::char_separator<char> sep(", ");
+  tokenizer tokens(field, sep);
+  BOOST_FOREACH ( std::string outNode, tokens ) {
+    result . push_back ( parseNodeName ( outNode, nodes_ ) );
+  }
+  return result;
+}
+
+inline NodeData 
+parseLine ( const std::string & line,
+            boost::unordered_map<std::string, int> & nodes_ ) {
+  //std::cout << "LINE: " << line << "\n";
+  NodeData result;
+  typedef boost::tokenizer<boost::char_separator<char> > tokenizer;
+  boost::char_separator<char> sep(":->");
+  tokenizer tokens(line, sep);
+  tokenizer::iterator tok_iter = tokens . begin ();
+  if ( tok_iter == tokens . end () ) throw std::logic_error ( line );
+  result . index = parseNodeName ( * tok_iter ++, nodes_ );
+  if ( tok_iter == tokens . end () ) throw std::logic_error ( line );
+  result . logic = parseProductOfSums ( * tok_iter ++, nodes_ );
+  if ( tok_iter == tokens . end () ) throw std::logic_error ( line );
+  result . out_order = parseOutOrder ( * tok_iter ++, nodes_ );
+  if ( tok_iter != tokens . end () ) throw std::logic_error ( line );
+  return result;
+}
+
+inline Network
+loadNetwork ( const char * filename ) {
+  Network result;
+  std::ifstream infile ( filename );
+  std::string line;
+  while ( std::getline ( infile, line ) ) {
+    try {
+      NodeData data = parseLine ( line, result . nodes_ );
+      result . node_data_ . push_back ( data );
+    } catch ( std::logic_error & except ) {
+      std::cout << "Failure parsing network file " << filename << "\n";
+      std::cout << "Trouble parsing: " << except . what () << "\n";
+      abort ();
+    }
+  }
+  infile . close ();
+  return result;
+}
+
+}
 
 #endif
