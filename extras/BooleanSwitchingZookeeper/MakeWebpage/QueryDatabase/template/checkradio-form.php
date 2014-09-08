b99b9ede2a5490b7c8faf5d1a877a57e1b056979
Arnaud Goullet <arnaud.goullet@gmail.com>
2014-09-08 12:05:41 -0400

    Switch to PHP Data Object (PDO) to handle access to the SQL database. Clean up the code and added the choice for the Hasse Diagram.

--- extras/BooleanSwitchingZookeeper/MakeWebpage/QueryDatabase/template/checkradio-form.php
+++ extras/BooleanSwitchingZookeeper/MakeWebpage/QueryDatabase/template/checkradio-form.php
@@ -1,97 +1,122 @@
 
 <?php
 
-// Open the SQL database
-$db = new SQLite3('graphs/database.sql');
 
-// basis for the query string 
-// need to retrieve the morse graph file id to fetch graphviz graphs
-$sqlquery="select distinct MORSEGRAPHFILEID from morsesets where ";
+try {
+	// Open database (create it if it does not exist)
+	$file_db = new PDO('sqlite:graphs/database.sql');
+	$file_db->setAttribute(PDO::ATTR_ERRMODE,
+     	   	               PDO::ERRMODE_EXCEPTION);
 
-$teststr='';
+	// basis for the query string 
+	// need to retrieve the morse graph file id to fetch graphviz graphs
+	$sqlquery="select distinct MORSEGRAPHFILEID from morsesets where ";
 
-$testYESstr='';
-$testNOstr='';
-$doingquery=false;
+	$doingquery=false;
 
-$intersectsqlstring=" intersect select MORSEGRAPHFILEID from morsesets where ";
-$countYES=0;
-$istring='';
+	$intersectsqlstring=" intersect select MORSEGRAPHFILEID from morsesets where ";
+	$istring='';
 
-// need to keep track of the properties 'N'
-$exceptsqlstring=" except select MORSEGRAPHFILEID from morsesets where ";
-$estring='';
-$countNO=0;
-$NoFound=false;
+	// need to keep track of the properties 'N'
+	$exceptsqlstring=" except select MORSEGRAPHFILEID from morsesets where ";
+	$estring='';
+	$NoFound=false;
 
-$countQuery=0;
+	$countQuery=0;
 
-foreach ( $_POST['radio'] as $message ) {
-	
-	$pos = strpos($message,':');
-	if ( $pos !== false ) {
-		// retieve the radio button status and symbol
-		$status = substr($message, 0, $pos);
-		$symbol = substr($message, $pos+1);
- 		if ( $status == "Y" ) {
-			$doingquery=true;
-			if ( $countQuery == 0 ) {
-				$sqlquery .= "\"".$symbol."\"=1";
-			} else {	
-				$istring .= $intersectsqlstring."\"".$symbol."\"=1";
-			}	
-			++$countQuery;
- 		}
- 		if ( $status == "N" ) {
-			$doingquery=true;
-			$NoFound=true;
-			if ( $countQuery == 0 ) {
-				$sqlquery .= "\"".$symbol."\"=0";
-			} else {	
-				$istring .= $intersectsqlstring."\"".$symbol."\"=0";
+	foreach ( $_POST['radio'] as $message ) {
+		
+		$pos = strpos($message,':');
+		if ( $pos !== false ) {
+			// retieve the radio button status and symbol
+			$status = substr($message, 0, $pos);
+			$symbol = substr($message, $pos+1);
+	 		if ( $status == "Y" ) {
+				$doingquery=true;
+				if ( $countQuery == 0 ) {
+					$sqlquery .= "\"".$symbol."\"=1";
+				} else {	
+					$istring .= $intersectsqlstring."\"".$symbol."\"=1";
+				}	
+				++$countQuery;
+	 		}
+	 		if ( $status == "N" ) {
+				$doingquery=true;
+				$NoFound=true;
+				if ( $countQuery == 0 ) {
+					$sqlquery .= "\"".$symbol."\"=0";
+				} else {	
+					$istring .= $intersectsqlstring."\"".$symbol."\"=0";
+				}
+				$estring .= $exceptsqlstring."\"".$symbol."\"=1 ";
+				++$countQuery;	
+	   	}
+		}
+	}
+
+	// Check the last radio button to retrieve the prefix for the filename fo the files to display
+	$filenamePrefix=end($_POST['radio']);
+
+	// if we ask for the Morse Graph Continuation Class
+	if ( $filenamePrefix == "MGCC" ) {
+
+		if ( $doingquery == true ) {
+			if ( $countQuery > 1 ) { 
+				$sqlquery .= $istring;
 			}
-			$estring .= $exceptsqlstring."\"".$symbol."\"=1 ";
-			++$countQuery;	
-   	}
+			// if selected a No, we need to append estring
+			if ( NOfound == true ) {
+				$sqlquery .= $estring;
+			}
+		} else {
+		// if no basic query, we select everything
+			$sqlquery="select distinct MORSEGRAPHFILEID from morsesets";
+		}
+
+		//echo "$sqlquery";
+
+		$results = $file_db->query($sqlquery) or die ('Query failed');
+
+		// Construct the Table 
+		echo "<table border=\"1\">";
+		$counter=0;
+		while ($row = $results->fetchArray()) {
+		  if ( $counter == 0 ) {
+		    echo "<tr>";
+		  }
+		  $MGfileindex=$row[MORSEGRAPHFILEID];
+		  echo "<td> <img src=\"graphs/MGCC$MGfileindex.png\" usemap=\"#MGCC{$MGfileindex}\" width=\"200\" > </td>";
+		  $counter = $counter + 1;
+		  if ( $counter==5 ) {
+		    echo "</tr>";
+		    $counter = 0;
+		  }
+		  // Introduce the "map" objects
+		  include ( "graphs/MGCC{$MGfileindex}-cmapx.html" );
+		}
+		echo "</table>";
+	} else { // if we asked instead for the Hasse Diagrams 
+		echo "<table border=\"1\">";
+		$counter=0;
+		// loop through all the files of the form HASSE*.png
+		foreach ( glob("graphs/HASSE*png") as $HasseFile ) {
+		  if ( $counter=0 ) {
+		        echo "<tr>";
+		  }
+		  echo "<td> <img src=\"$HasseFile\" width=\"200\" > </td>";
+		  $counter = $counter + 1;
+		  if ( $counter==5 ) {
+		    echo "</tr>";
+		    $counter = 0;
+		  }
+		}
+		echo "</table>";
 	}
+	$file_db -> close(); 
 }
-
-if ( $doingquery == true ) {
-	if ( $countQuery > 1 ) { 
-		$sqlquery .= $istring;
-	}
-	// if selected a No, we need to append estring
-	if ( NOfound == true ) {
-		$sqlquery .= $estring;
-	}
-} else {
-// if no basic query, we select everything
-	$sqlquery="select distinct MORSEGRAPHFILEID from morsesets";
+catch (PDOException $e) {
+        echo $e->getMessage();
 }
-
-//echo "$sqlquery";
-
-$results = $db->query($sqlquery) or die ('Query failed');
-
-// Construct the Table 
-echo "<table border=\"1\">";
-$counter=0;
-while ($row = $results->fetchArray()) {
-  if ( $counter == 0 ) {
-    echo "<tr>";
-  }
-  $MGfileindex=$row[MORSEGRAPHFILEID];
-  echo "<td> <img src=\"graphs/MGCC$MGfileindex.png\" usemap=\"#MGCC{$MGfileindex}\" width=\"200\" > </td>";
-  $counter = $counter + 1;
-  if ( $counter==5 ) {
-    echo "</tr>";
-    $counter = 0;
-  }
-  // Introduce the "map" objects
-  include ( "graphs/MGCC{$MGfileindex}-cmapx.html" );
-}
-echo "</table>";
-
 
 ?>
 
