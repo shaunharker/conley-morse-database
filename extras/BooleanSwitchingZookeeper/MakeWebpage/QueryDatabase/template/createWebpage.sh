a4eb104c5811820cabc3bb319c0f4cc813d26ea3
Arnaud Goullet <arnaud.goullet@gmail.com>
2014-09-08 13:22:11 -0400

    Updated the script to generate SQL website with Hasse diagram radio button

--- extras/BooleanSwitchingZookeeper/MakeWebpage/QueryDatabase/template/createWebpage.sh
+++ extras/BooleanSwitchingZookeeper/MakeWebpage/QueryDatabase/template/createWebpage.sh
@@ -14,7 +14,7 @@ printf "<html> \n <head> \n <title> $2 </title> \n </head> \n <body> \n" > $file
 
 # for the radio buttons
 printf "<form action=\"checkradio-form.php\" method=\"post\" target=\"MGframe\">\n" >> $file
-printf "Find the Morse graph that contains at least one Morse set with the following properties : <br />\n" >> $file
+printf "<p>Find the Morse graph that contains at least one Morse set with the following properties :</p> <br />\n" >> $file
 printf "<table border=\"1\">\n" >> $file
 # columns title
 printf "<tr> \n <th> Yes </th> \n <th> No </th> \n <th> Either </th> \n</tr> \n" >> $file
@@ -28,7 +28,7 @@ while read line; do
 #   #define C**** 
     string=`echo $line | grep '^#define C' | cut -d'"' -f2`
     if [ -n "$string" ]; then
-    	counter=$(( $counter+1 )) >> $file
+    	counter=$(( $counter+1 )) # >> $file
 	    printf "<tr> \n" >> $file
 	    printf "<td> <input type=\"radio\" name=\"radio["$counter"]\" " >> $file
 	    # value convention (colon separated) ->  status : symbol
@@ -56,8 +56,22 @@ while read line; do
 	    printf "</tr>" >> $file
 	  fi
 done < $sourcedir/AnnotationConditions.h
-
 printf "</table>\n" >> $file
+
+# add the radio button to handle the choice MGCC or Hasse diagrams
+counter=$(( $counter+1 ))
+printf "<p> Choose the output : </p> <br>\n" >> $file
+printf "<table border=\"1\">" >> $file
+printf "<tr>\n" >> $file
+printf "<td> <input type=\"radio\" name=\"radio[$counter]\" value=\"MGCC\" checked /> </td>\n" >> $file
+printf "<td> Morse Graph Continuation Classes that occur, sorted by frequency </td> \n" >> $file
+printf "</tr>" >> $file
+printf "<tr>" >> $file 
+printf "<td> <input type=\"radio\" name=\"radio[$counter]\" value=\"HASSE\" /> </td> \n" >> $file
+printf "<td> Hasse Diagram Zoo, sorted by frequency </td>\n" >> $file
+printf "</tr>\n" >> $file
+printf "</table>\n" >> $file
+
 printf "<input type=\"submit\" name=\"formSubmit\" value=\"Submit\" />\n" >> $file
 printf "</form>\n" >> $file
 
