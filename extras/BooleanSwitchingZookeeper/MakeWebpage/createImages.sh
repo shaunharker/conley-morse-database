0d8461767259db2706dc5826991db829e9b9d26a
Arnaud Goullet <goullet@conley3.rutgers.edu>
2014-12-12 12:41:12 -0500

    bug fix in creating images

--- extras/BooleanSwitchingZookeeper/MakeWebpage/createImages.sh
+++ extras/BooleanSwitchingZookeeper/MakeWebpage/createImages.sh
@@ -11,14 +11,20 @@ databasedir=$1
 # extract the name of the directory
 # Remark: The name of the directory is used as a prefix for some of the
 # file names
-dirname=`echo $databasedir | rev | cut -d'/' -f1 | rev`
-
+# check if the path was given with an ending "/" or not 
+lastchar=`echo "${databasedir:$((${#databasedir}-1)):1}"`
+if [[ "$lastchar" == "/" ]]
+then
+	dirname=`echo $databasedir | rev | cut -d'/' -f2 | rev`
+else 
+	dirname=`echo $databasedir | rev | cut -d'/' -f1 | rev`
+fi
 # create the png for the network equations
-		a2ps --no-header --border=no "$databasedir/$dirname.txt" -o test.ps
-	  ps2eps --rotate=+ test.ps
-	  convert -density 300 -quality 100 test.eps test.png
-	  mv test.png "$databasedir/equations.png"
-	  rm test.*
+	a2ps --no-header --border=no "$databasedir/$dirname.txt" -o test.ps
+	ps2eps --rotate=+ test.ps
+	#convert -density 300 -quality 100 test.eps test.png
+	gs -dSAFER -dEPSCrop -dBATCH -dNOPAUSE -r600 -sDEVICE=pnggray -sOutputFile="$databasedir/equations.png" "test.eps"
+	rm test.*
 # convert the txt file into a graphviz file and then .png
-	  ./convertTXTtoGV.sh "$databasedir/${dirname}.txt"
-	  dot "$databasedir/${dirname}.gv" -Tpng -o"$databasedir/network.png"
+	./convertNetworkFileTXTtoGV.sh "$databasedir/${dirname}.txt"
+	dot "$databasedir/${dirname}.gv" -Tpng -o"$databasedir/network.png"
