6bc54c5ea920481ef4388f9fa770e0ae675797e6
Arnaud Goullet <arnaud.goullet@gmail.com>
2015-01-19 14:43:40 -0500

    The parameter graph for a given MGCC is now part of the output when extracting a particular morse graph class.

--- extras/BooleanSwitchingExtractMorseSet/main.cpp
+++ extras/BooleanSwitchingExtractMorseSet/main.cpp
@@ -39,38 +39,41 @@ BOOST_CLASS_EXPORT_IMPLEMENT(AbstractParameterSpace);
 int main ( int argc, char * argv [] ) {
   Database database;
   database . load ( argv [ 1 ] );
-  
+
   const AbstractParameterSpace & space =
   dynamic_cast<const AbstractParameterSpace&> (database . parameter_space ());
-  
+
   boost::unordered_set < uint64_t > parameters;
   boost::unordered_set < uint64_t > mgr_indices;
-  
-  
+
+
   const std::vector < MGCC_Record > & mgcc_records =
   database . MGCC_Records ();
-  
+
   std::cout << "Number of records : " << mgcc_records.size() << "\n";
-  
+
   // the index of interest comes from the last argument
   uint64_t mgcc;
   mgcc = atoi(argv[argc-1]);
-  
+
   // we take the first parameter index from that particular record
   const MGCCP_Record & mgccp_record =
   database . MGCCP_Records () [ mgcc_records[mgcc].mgccp_indices[0] ];
+
+  std::cout << "Number of records for the given MGCC : " << mgcc_records[mgcc].mgccp_indices.size() << "\n";
+
   uint64_t morsegraph_index = mgccp_record . morsegraph_index;
   mgr_indices . insert ( morsegraph_index );
   const MorseGraphRecord & morsegraph_record =
   database . morsegraphData () [ morsegraph_index ];
-  
+
   uint64_t dag_index = morsegraph_record . dag_index;
   const DAG_Data & dag_data = database . dagData () [ dag_index ];
-  
+
   std::cout << "Number of vertices : " << dag_data . num_vertices << "\n";
-  
+
   std::vector < uint64_t > pindex = mgccp_record . parameter_indices;
-  
+
   //
   Model model;
   model . initialize ( argc-1, argv+1 );
@@ -79,10 +82,37 @@ int main ( int argc, char * argv [] ) {
   boost::dynamic_pointer_cast<BooleanSwitchingParameterSpace> (
                                                                model . parameterSpace () );
   //
- 
- 
+
+// Output the parameter graph for the given MGCC number
+
 std::cout << "Number of parameters : " << pindex.size() << "\n";
- 
+
+  std::ofstream myfile;
+  myfile . open ( "parameterGraphForaGivenMGCC.gv" );
+  myfile << "graph {\n";
+ for ( unsigned int i=0; i<pindex.size(); ++i ) {
+   std::cout << "i= " << i << " : " << pindex[i] << "\n";
+   myfile << pindex[i] <<";\n";
+ }
+
+ for ( unsigned int i=0; i<pindex.size()-1; ++i ) {
+   uint64_t p1 = pindex[i];
+   for ( unsigned int j=i+1; j<pindex.size(); ++j ) {
+     uint64_t p2 = pindex[j];
+     uint64_t x ( p1 ^ p2 );
+     if ( (x != 0) && !(x & (x - 1)) ) { // check it is a power of 2, i.e. only a single 1 in the bit sequence
+      //  std::cout << p1 << " " << p2 << " are neighbors\n";
+       myfile << p1 << "--" << p2 << ";\n";
+     } else {
+      //  std::cout << p1 << " " << p2 << " are not neighbors\n";
+     }
+   }
+ }
+ myfile << "}";
+ myfile.close();
+
+ //
+
   // pick one paramater index to construct p
   boost::shared_ptr<BooleanSwitchingParameter> p =
   boost::dynamic_pointer_cast<BooleanSwitchingParameter> (
@@ -90,9 +120,9 @@ std::cout << "Number of parameters : " << pindex.size() << "\n";
   //  std::cout << "Parameters inequalities : \n";
   std::ofstream parameterfile;
   parameterfile . open ( "parametersInequalities.txt" );
-    parameterfile << boolean_space . prettyPrint ( boolean_space . parameter ( pindex[0] ) );
+  parameterfile << boolean_space . prettyPrint ( boolean_space . parameter ( pindex[0] ) );
   parameterfile . close();
-  
+
   //
   boost::shared_ptr<const Map> map = model . map ( p );
   if ( not map ) {
@@ -104,7 +134,7 @@ std::cout << "Number of parameters : " << pindex.size() << "\n";
     throw std::logic_error ( "Clutching_Graph_Job. model.phaseSpace() failed"
                             " to return a valid pointer.\n");
   }
-  
+
   // Perform Morse Graph computation
   MorseGraph mg;
   Compute_Morse_Graph
@@ -115,30 +145,30 @@ std::cout << "Number of parameters : " << pindex.size() << "\n";
    0,
    0,
    0 );
-  
-  
+
+
   model . annotate ( & mg );
-  
+
   std::cout << "Computed a Morse graph with "
   << mg . NumVertices () << " nodes.\n";
-  
+
   //
   std::ofstream ofile;
   ofile.open("morseSets.txt");
-  
+
   typedef std::vector<Grid::GridElement> CellContainer;
   typedef  MorseGraph::VertexIterator VI;
   VI it, stop;
   for (boost::tie ( it, stop ) = mg . Vertices (); it != stop;  ++ it ) {
-    
+
     boost::shared_ptr<const Grid> my_subgrid ( mg . grid ( *it ) );
-    
+
     if ( not my_subgrid ) {
       std::cout << "Abort! This vertex does not have an associated grid!\n";
       abort ();
     }
     CellContainer my_subset = phase_space -> subset ( * my_subgrid );
-    
+
     BOOST_FOREACH ( Grid::GridElement ge, my_subset ) {
       if ( not boost::dynamic_pointer_cast < AtlasGeo > ( phase_space -> geometry ( ge ) ) ) {
         std::cout << "Unexpected null response from geometry\n";
@@ -147,24 +177,24 @@ std::cout << "Number of parameters : " << pindex.size() << "\n";
       RectGeo box =  geo . rect ();
       int id = geo . id ();
       // std::cout << "(Chart, Rect) = (" << id << ", " << box << ")\n";
-      
+
       ofile << *it << " " << id ;//  << " ): " << box << "\n";
-      
+
       ofile << "\n";
-      
+
     }
   }
-  
+
   ofile.close();
-  
-  
+
+
   // Extract the coorespondence network node and variable names
   BooleanSwitching::Network network;
   std::string str1 = argv[2];
   std::string str2 = argv[3];
   std::string str3 = str1 + "/" + str2;
   network.load(str3.c_str());
-  
+
   std::ofstream variablefile;
   variablefile . open ( "variables.txt" );
   // index starts at 1
@@ -172,6 +202,6 @@ std::cout << "Number of parameters : " << pindex.size() << "\n";
     variablefile << i-1 << " " << network.name(i) << "\n";
   }
   variablefile.close();
-  
+
   return 0;
 }
